d62121586514968369454e34c45e74b1619d500f
Khaliva <abdukhaliva@gmail.com>
2026-08-14 14:49:07 +0300

    Create TheLazyStartupOffice.js

---
+++ TheLazyStartupOffice.js
@@ -0,0 +1,16 @@
+function binRota(arr){
+    let result = [];
+    let currentTick = 0;
+    for (let i = 0; i < arr.length; i++) {
+            if (currentTick % 2 == 0) {
+        result.push(...arr[currentTick])
+    }
+    else{
+        let oppoRes = arr[i].reverse();
+        result.push(...arr[oppoRes])
+    }
+    currentTick++;
+    }
+    return result;
+
+}
